6a545c3dd062a6c96b7e382707394701f90464ef
CaptionWeed <cyj92016@gmail.com>
2023-09-27 16:47:50 +0800

    update Room,Room_ord,Room_photo,Room_stock

--- src/main/java/com/tha103/gogoyu/room_ord/model/Room_ordDAO_interface.java
+++ src/main/java/com/tha103/gogoyu/room_ord/model/Room_ordDAO_interface.java
@@ -4,10 +4,10 @@ import java.util.*;
 
 
 public interface Room_ordDAO_interface {
-	public void insert(Room_ord room_ord);
-	public void update(Room_ord room_ord);
-	public void delete(Integer room_ord_id);
-	public Room_ord findByPrimaryKey(Integer room_ord_id);
+	public void insert(Room_ord roomOrd);
+	public void update(Room_ord roomOrd);
+	public void delete(Integer roomOrdId);
+	public Room_ord findByPrimaryKey(Integer roomOrdId);
 	public List<Room_ord> getAll();
 	
 
