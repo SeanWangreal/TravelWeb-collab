b51c44fd513fafe4e381f4fe08794d2ca9c1b2c9
HolloWu <hollowu0228@gmail.com>
2023-10-08 14:06:21 +0800

    Trip_ordVO GenerationType.IDENTITY update

--- src/main/java/com/tha103/gogoyu/trip_ord/model/Trip_ord.java
+++ src/main/java/com/tha103/gogoyu/trip_ord/model/Trip_ord.java
@@ -6,6 +6,8 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -15,6 +17,7 @@ public class Trip_ord implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="trip_ord_id")
 	private Integer tripOrdId;
 	@Column(name="trip_id")
