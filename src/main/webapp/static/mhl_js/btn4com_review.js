7c1c18d8bfd95797c4ff1c1802b1a12a88a9160a
Ming Han Li <ssss64642@gmail.com>
2023-10-14 22:29:43 +0800

    webapp資料夾新增html、js和css 1.新增mhl資料夾，放入未完成的html 2.static資料夾內新增mhl_css和mhl_js資料夾，放入html使用的js和css

---
+++ src/main/webapp/static/mhl_js/btn4com_review.js
@@ -0,0 +1,68 @@
+var msg_btn = document.getElementById("msg");
+var msg_side = document.getElementById("msg_side");
+var info_btn = document.getElementById("info");
+var info_side = document.getElementById("info_side");
+var shop_btn = document.getElementById("shop");
+var shop_side = document.getElementById("shop_side");
+var add_btn = document.getElementById("add");
+var yes_btn = document.getElementById("yes");
+var no_btn = document.getElementById("no");
+var bg_alert = document.querySelector(".alert_bg")
+var al = document.querySelector(".alert");
+
+function say() {
+info_side.classList.remove("on");
+shop_side.classList.remove("on");
+msg_side.classList.toggle("on");
+}
+
+function info() {
+msg_side.classList.remove("on");
+shop_side.classList.remove("on");
+info_side.classList.toggle("on");
+}
+function add(e) {
+    console.log("s");
+    bg_alert.classList.add("on");
+    al.classList.add("on");
+    body = document.querySelector("body");
+    body.classList.add("stop");
+}
+
+function no() {
+    bg_alert.classList.remove("on");
+    al.classList.remove("on");
+    body.classList.remove("stop");
+}
+function yes() {
+    $("#add-data").click();
+}
+
+msg_btn.addEventListener("click", say);
+info_btn.addEventListener("click", info);
+$(function(){
+    $(document).on("click",function(e){
+        if(!$(e.target).hasClass("nothing") && !$(e.target).hasClass("head_btn") && 
+                                                    !$(e.target).hasClass("icon")){
+            $(".all_side").removeClass("on");
+        }
+    })
+    if(add_btn != null){
+        add_btn.addEventListener("click", add);
+    }
+    if (no_btn != null){
+        no_btn.addEventListener("click", no);
+    }
+    if (yes_btn != null){
+        yes_btn.addEventListener("click", yes);
+    }
+    $(".read").on('click',function(){
+        var p = $(this).closest(".review-block").find("p");
+        p.toggleClass("zoom");
+        if (p.hasClass('zoom')){
+            $(this).text("收回")
+        } else{
+            $(this).text("查看全部")
+        }
+    })
+})
