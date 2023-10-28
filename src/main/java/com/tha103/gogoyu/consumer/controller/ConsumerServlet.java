515c178eb02b85f3a5fca1260140b431e241a64b
CaptionWeed <cyj92016@gmail.com>
2023-10-28 17:36:50 +0800

    commit

--- src/main/java/com/tha103/gogoyu/consumer/controller/ConsumerServlet.java
+++ src/main/java/com/tha103/gogoyu/consumer/controller/ConsumerServlet.java
@@ -11,8 +11,16 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
-
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -31,13 +39,59 @@ import com.tha103.gogoyu.planning.model.PlanningServiceHibernate;
 @WebServlet("/eric/ConsumerServlet")
 @MultipartConfig(fileSizeThreshold = 1024 * 1024, maxFileSize = 5 * 1024 * 1024, maxRequestSize = 5 * 5 * 1024 * 1024)
 public class ConsumerServlet extends HttpServlet {
+	
+	public void sendMail(String to, String subject, String messageText) {
+
+		try {
+			// 設定使用SSL連線至 Gmail smtp Server
+			Properties props = new Properties();
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.socketFactory.port", "465");
+			props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.port", "465");
+
+	        // ●設定 gmail 的帳號 & 密碼 (將藉由你的Gmail來傳送Email)
+	        // ●1) 登入你的Gmail的: 
+	        // ●2) 點選【管理你的 Google 帳戶】
+	        // ●3) 點選左側的【安全性】
+	       
+	        // ●4) 完成【兩步驟驗證】的所有要求如下:
+	        //     ●4-1) (請自行依照步驟要求操作之.....)
+	       
+	        // ●5) 完成【應用程式密碼】的所有要求如下:
+	        //     ●5-1) 下拉式選單【選取應用程式】--> 選取【郵件】
+	        //     ●5-2) 下拉式選單【選取裝置】--> 選取【Windows 電腦】
+	        //     ●5-3) 最後按【產生】密碼
+			final String myGmail = "cyj92016@gmail.com";
+			final String myGmail_password = "adsl123123";
+			Session session = Session.getInstance(props, new Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(myGmail, myGmail_password);
+				}
+			});
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(myGmail));
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+
+			// 設定信中的主旨
+			message.setSubject(subject);
+			// 設定信中的內容
+			message.setText(messageText);
+
+			Transport.send(message);
+			System.out.println("傳送成功!");
+		} catch (MessagingException e) {
+			System.out.println("傳送失敗!");
+			e.printStackTrace();
+		}
+	}
 
 	public static InputStream getPictureStream(String path) throws IOException {
 		FileInputStream fis = new FileInputStream(path);
 		return fis;
 	}
-
-	
 	public static byte[] getPictureByteArray(String path) throws IOException {
 		FileInputStream fis = new FileInputStream(path);
 		byte[] buffer = new byte[fis.available()];
@@ -45,68 +99,14 @@ public class ConsumerServlet extends HttpServlet {
 		fis.close();
 		return buffer;
 	}
-
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		doPost(req, res);
 	}
-
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("text/plain; charset=UTF-8");
 		req.setCharacterEncoding("UTF-8");
 		String action = req.getParameter("action");
 		ConsumerServiceHibernate cusSvc = new ConsumerServiceHibernate();
-
-//		if ("getOne_For_Display".equals(action)) { // 來自select_page.jsp的請求
-//
-//			List<String> errorMsgs = new LinkedList<String>();
-//			// Store this set in the request scope, in case we need to
-//			// send the ErrorPage view.
-//			req.setAttribute("errorMsgs", errorMsgs);
-//
-//			/*************************** 1.接收請求參數 - 輸入格式的錯誤處理 **********************/
-//			String str = req.getParameter("cusId");
-//			if (str == null || (str.trim()).length() == 0) {
-//				errorMsgs.add("請輸入會員編號");
-//			}
-//			// Send the use back to the form, if there were errors
-//			if (!errorMsgs.isEmpty()) {
-//				RequestDispatcher failureView = req.getRequestDispatcher("/eric/select_page.jsp");
-//				failureView.forward(req, res);
-//				return;// 程式中斷
-//			}
-//
-//			Integer cusId = null;
-//			try {
-//				cusId = Integer.valueOf(str);
-//			} catch (Exception e) {
-//				errorMsgs.add("員工編號格式不正確");
-//			}
-//			// Send the use back to the form, if there were errors
-//			if (!errorMsgs.isEmpty()) {
-//				RequestDispatcher failureView = req.getRequestDispatcher("/eric/select_page.jsp");
-//				failureView.forward(req, res);
-//				return;// 程式中斷
-//			}
-//
-//			/*************************** 2.開始查詢資料 *****************************************/
-////			ConsumerServiceHibernate cusSvc = new ConsumerServiceHibernate();
-//			Consumer consumer = cusSvc.getOneCus(cusId);
-//			if (consumer == null) {
-//				errorMsgs.add("查無資料");
-//			}
-//			// Send the use back to the form, if there were errors
-//			if (!errorMsgs.isEmpty()) {
-//				RequestDispatcher failureView = req.getRequestDispatcher("/eric/select_page.jsp");
-//				failureView.forward(req, res);
-//				return;// 程式中斷
-//			}
-//
-//			/*************************** 3.查詢完成,準備轉交(Send the Success view) *************/
-//			req.setAttribute("consumer", consumer); // 資料庫取出的empVO物件,存入req
-//			String url = "/eric/listOneCus.jsp";
-//			RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
-//			successView.forward(req, res);
-//		}
 
 		if ("getOne_For_Login".equals(action)) { // 來自select_page.jsp的請求
 
@@ -618,6 +618,57 @@ public class ConsumerServlet extends HttpServlet {
 //			/*************************** 3.刪除完成,準備轉交(Send the Success view) ***********/
 //			String url = "/eric/listAllCus.jsp";
 //			RequestDispatcher successView = req.getRequestDispatcher(url);// 刪除成功後,轉交回送出刪除的來源網頁
+//			successView.forward(req, res);
+//		}
+//		if ("getOne_For_Display".equals(action)) { // 來自select_page.jsp的請求
+//
+//			List<String> errorMsgs = new LinkedList<String>();
+//			// Store this set in the request scope, in case we need to
+//			// send the ErrorPage view.
+//			req.setAttribute("errorMsgs", errorMsgs);
+//
+//			/*************************** 1.接收請求參數 - 輸入格式的錯誤處理 **********************/
+//			String str = req.getParameter("cusId");
+//			if (str == null || (str.trim()).length() == 0) {
+//				errorMsgs.add("請輸入會員編號");
+//			}
+//			// Send the use back to the form, if there were errors
+//			if (!errorMsgs.isEmpty()) {
+//				RequestDispatcher failureView = req.getRequestDispatcher("/eric/select_page.jsp");
+//				failureView.forward(req, res);
+//				return;// 程式中斷
+//			}
+//
+//			Integer cusId = null;
+//			try {
+//				cusId = Integer.valueOf(str);
+//			} catch (Exception e) {
+//				errorMsgs.add("員工編號格式不正確");
+//			}
+//			// Send the use back to the form, if there were errors
+//			if (!errorMsgs.isEmpty()) {
+//				RequestDispatcher failureView = req.getRequestDispatcher("/eric/select_page.jsp");
+//				failureView.forward(req, res);
+//				return;// 程式中斷
+//			}
+//
+//			/*************************** 2.開始查詢資料 *****************************************/
+////			ConsumerServiceHibernate cusSvc = new ConsumerServiceHibernate();
+//			Consumer consumer = cusSvc.getOneCus(cusId);
+//			if (consumer == null) {
+//				errorMsgs.add("查無資料");
+//			}
+//			// Send the use back to the form, if there were errors
+//			if (!errorMsgs.isEmpty()) {
+//				RequestDispatcher failureView = req.getRequestDispatcher("/eric/select_page.jsp");
+//				failureView.forward(req, res);
+//				return;// 程式中斷
+//			}
+//
+//			/*************************** 3.查詢完成,準備轉交(Send the Success view) *************/
+//			req.setAttribute("consumer", consumer); // 資料庫取出的empVO物件,存入req
+//			String url = "/eric/listOneCus.jsp";
+//			RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
 //			successView.forward(req, res);
 //		}
 		
