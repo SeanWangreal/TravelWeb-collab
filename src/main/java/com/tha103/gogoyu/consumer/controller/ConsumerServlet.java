ebdc776003a5cb442577efc26200861143a156a4
CaptionWeed <cyj92016@gmail.com>
2023-10-31 20:41:53 +0800

    更新好驗證信的顯示

--- src/main/java/com/tha103/gogoyu/consumer/controller/ConsumerServlet.java
+++ src/main/java/com/tha103/gogoyu/consumer/controller/ConsumerServlet.java
@@ -144,12 +144,15 @@ public class ConsumerServlet extends HttpServlet {
 			/*************************** 1.接收請求參數 - 輸入格式的錯誤處理 **********************/
 			String mail = req.getParameter("cusMail");
 			String mailReg = "\\w[-\\w.+]*@([A-Za-z0-9][-A-Za-z0-9]+\\.)+[A-Za-z]{2,14}";
+			HttpSession session = req.getSession();
 
 			if (mail == null || (mail.trim()).length() == 0) {
 				errorMsgs.add("請輸入信箱");
 			}else if (!mail.trim().matches(mailReg)) { // 以下練習正則(規)表示式(regular-expression)
 				errorMsgs.add("信箱格式錯誤 範例XXXX@XXX.XXX");
 			}
+			session.setAttribute("mail", mail);
+
 			
 			// Send the use back to the form, if there were errors
 			if (!errorMsgs.isEmpty()) {
@@ -164,7 +167,6 @@ public class ConsumerServlet extends HttpServlet {
 			String subject = "歡迎註冊GOGOYU會員";
 
 			String passRandom = generateVerificationCode();
-			HttpSession session = req.getSession();
 			session.setAttribute("passRandom", passRandom);
 			System.out.println(passRandom);
 
